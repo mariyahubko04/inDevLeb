65e5624ec048b4174be12931b3fa5fecd6e36fd5
Mariya Hubko <48643833+mariyahubko04@users.noreply.github.com>
2019-08-14 15:57:16 +0300

    Update LangPairings.js

--- src/components/LangPairings.js
+++ src/components/LangPairings.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const LangPairings = () => (
   <div class="main__lang-column">
+  
     <div className="main__lang-column--section">
       <h2>
         <span className="main__upload--number">2</span>
         Choose your languages pairings
-          </h2>
+       </h2>
     </div>
+  
     <form className="main__nav-lang">
+  
       <div className="main__nav-lang-section">
         <span className="main__nav-lang-text">FROM</span>
         <select className="main__nav-lang-input">
@@ -17,6 +20,7 @@ const LangPairings = () => (
           <option value="УКРАЇНСЬКА">Українська</option>
         </select>
       </div>
+  
       <div className="main__nav-lang-section">
         <span className="main__nav-lang-text">TO</span>
         <select className="main__nav-lang-input">
@@ -26,7 +30,9 @@ const LangPairings = () => (
           <option value="УКРАЇНСЬКА">Українська</option>
         </select>
       </div>
+  
     </form>
+  
   </div>
 )
 
